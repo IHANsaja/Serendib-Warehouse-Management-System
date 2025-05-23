d753392da1d8906946817e89fd985f6390387f9a
Vibhusha Gangulel <vibhushananayakkara@gmail.com>
2025-05-23 15:17:55 +0530

    Login completed

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,15 @@
+// server.js
 const express = require('express');
-const mysql = require('mysql2');
 const cors = require('cors');
-require('dotenv').config()
-
+require('dotenv').config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-
-//MySQL Connection
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: "",
-    database: process.env.DB_NAME,
-});
-
-
-//Connect DB
-db.connect((err) => {
-    if(err){
-        console.error('DB connection failed: ', err);
-    } else {
-        console.log('Connected to MySQL');
-    }
-});
-
-// Start Server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
 
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
 
-module.exports = db;
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
